607234f1a4121602c232ce6d74d70dddddfc8918
LucasFreitasRocha <rochadefreitaslucas@gmail.com>
2022-07-27 00:09:48 -0300

    teste e swagger

--- src/main/java/com/ta2/probechallenge/controller/ProbeController.java
+++ src/main/java/com/ta2/probechallenge/controller/ProbeController.java
@@ -3,6 +3,8 @@ package com.ta2.probechallenge.controller;
 import com.ta2.probechallenge.dto.in.CommandDTO;
 import com.ta2.probechallenge.model.Probe;
 import com.ta2.probechallenge.service.ProbeService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,8 +16,10 @@ public class ProbeController {
     @Autowired private ProbeService service;
 
     @PostMapping("/{id}")
-    public ResponseEntity<Probe> moveProbe(@PathVariable Long id, @RequestBody CommandDTO commandDTO){
-        System.out.println("Iniciou");
+    public ResponseEntity<Probe> moveProbe(
+            @PathVariable
+            @ApiParam(name = "id", value = "Id of Probe", example = "1") Long id,
+            @RequestBody CommandDTO commandDTO){
         return ResponseEntity.ok(service.instruction(id,commandDTO));
     }
 }
---
+++ src/test/java/com/ta2/probechallenge/service/ProbeServiceTest.java
@@ -0,0 +1,96 @@
+package com.ta2.probechallenge.service;
+
+import com.ta2.probechallenge.dto.in.CommandDTO;
+import com.ta2.probechallenge.model.Probe;
+import com.ta2.probechallenge.repository.ProbeRepository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ProbeServiceTest {
+    @InjectMocks
+    private ProbeService service;
+
+
+    @Mock
+    private ProbeRepository repository;
+
+    private Long id = Long.valueOf(1) ;
+
+    private final String RESPONSETHROW = "No row with the given identifier exists:" +
+            " [Sonda não encontrada com esse id: 1#1]";
+
+
+
+
+    @Test
+    void findOk() {
+        Probe expect = getProbe();
+        when(repository.findById(id)).thenReturn(Optional.of(expect)) ;
+        Probe response = service.find(id);
+        assertEquals(expect.getPosition(),response.getPosition() );
+        assertEquals(expect.getX(),response.getX() );
+        assertEquals(expect.getY(), response.getY());
+        assertEquals(expect.getName(), response.getName());
+        assertEquals(expect.getId(), response.getId());
+    }
+
+    @Test
+    void findThrow() {
+        when(repository.findById(id)).thenReturn(Optional.empty()) ;
+       try{
+           service.find(id);
+       }catch (Exception e){
+           assertEquals(RESPONSETHROW, e.getMessage());
+       }
+    }
+
+    @Test
+    void moveWithSingleCommandWhitProbe1(){
+        when(repository.findById(id)).thenReturn(Optional.of(getProbe())) ;
+        Probe response = service.instruction(id,getCommandDTO("M"));
+        assertEquals(3, response.getY());
+
+    }
+
+    @Test
+    void command1Probe1(){
+        when(repository.findById(id)).thenReturn(Optional.of(getProbe())) ;
+        Probe response = service.instruction(id,getCommandDTO("LMLMLMLMM"));
+        assertEquals(1, response.getX());
+        assertEquals(3, response.getY());
+        assertEquals("N", response.getPosition());
+    }
+
+    public CommandDTO getCommandDTO(String command){
+        CommandDTO commandDTO = new CommandDTO();
+        commandDTO.setCommand(command);
+        return  commandDTO;
+    }
+
+    private Probe getProbe() {
+        Probe probe = new Probe();
+        probe.setId(id);
+        probe.setPosition("N");
+        probe.setX(1);
+        probe.setY(2);
+        probe.setName("mars rover");
+        return  probe;
+    }
+
+    @Test
+    void instruction() {
+
+    }
+}
