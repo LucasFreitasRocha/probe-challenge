3a58b8ce76e316fac32f1f13b9c6ad995a803c3e
Lucas Freitas da Rocha <rochadefreitaslucas@gmail.com>
2023-10-02 15:57:44 -0300

    update spring

--- src/main/java/com/ta2/probechallenge/entity/ProbeEntity.java
+++ src/main/java/com/ta2/probechallenge/entity/ProbeEntity.java
@@ -1,12 +1,13 @@
 package com.ta2.probechallenge.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+
 
 @Data
 @NoArgsConstructor
--- src/main/java/com/ta2/probechallenge/service/ProbeService.java
+++ src/main/java/com/ta2/probechallenge/service/ProbeService.java
@@ -43,6 +43,7 @@ public class ProbeService {
                         ));
             }
         });
+
         return probeDomain;
     }
 
