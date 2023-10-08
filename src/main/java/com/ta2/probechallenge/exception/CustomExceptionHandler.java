f6139dbd31989a46e5b7943305df8d6ff8a06576
Lucas Freitas da Rocha <rochadefreitaslucas@gmail.com>
2023-10-08 12:47:41 -0300

    fix name

--- src/main/java/com/ta2/probechallenge/exception/CustomExceptionHandler.java
+++ src/main/java/com/ta2/probechallenge/exception/CustomExceptionHandler.java
@@ -1,12 +1,10 @@
 package com.ta2.probechallenge.exception;
 
-import com.fasterxml.jackson.core.JsonParseException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,7 +16,7 @@ import java.util.UUID;
 
 @ControllerAdvice
 @RequiredArgsConstructor
-public class CustomExeceptionHandler {
+public class CustomExceptionHandler {
 
 
 
@@ -61,7 +59,7 @@ public class CustomExeceptionHandler {
     }
 
     @ExceptionHandler({Exception.class})
-    public ResponseEntity<CustomError> handleGenericExecption(Exception ex, HttpServletRequest request) {
+    public ResponseEntity<CustomError> handleGenericException(Exception ex, HttpServletRequest request) {
         List<ErroInformation> erroInformation = new ArrayList<>();
         erroInformation.add(new ErroInformation("INTERNAL_SERVER_ERROR", ex.getMessage()));
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
