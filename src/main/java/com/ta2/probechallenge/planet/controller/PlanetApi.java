576054b89645ab5555aeb8a21cddf2dba4139b11
Lucas Freitas da Rocha <rochadefreitaslucas@gmail.com>
2023-10-09 19:00:50 -0300

    operation in planetController

--- src/main/java/com/ta2/probechallenge/planet/controller/PlanetApi.java
+++ src/main/java/com/ta2/probechallenge/planet/controller/PlanetApi.java
@@ -3,6 +3,7 @@ package com.ta2.probechallenge.planet.controller;
 import com.ta2.probechallenge.planet.dto.in.PlanetCreateDto;
 import com.ta2.probechallenge.planet.dto.out.ListPlanetDto;
 import com.ta2.probechallenge.planet.dto.out.PlanetDto;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -15,13 +16,14 @@ import java.util.UUID;
 @RequestMapping("/planet/v1")
 @Tag(name = "planet")
 public interface PlanetApi {
-
+    @Operation(summary = "create a planet")
     @PostMapping
     public ResponseEntity<PlanetDto> create(@RequestBody @Valid PlanetCreateDto planetCreateDto);
-
+    @Operation(summary = "list planets")
     @GetMapping
     public ResponseEntity<List<ListPlanetDto>> findAll();
 
+    @Operation(summary = "update information of a planet")
     @PatchMapping("/{id}")
     public ResponseEntity<PlanetDto> update(
             @PathVariable
@@ -30,11 +32,12 @@ public interface PlanetApi {
             @Valid @RequestBody PlanetCreateDto planetCreateDto
     );
 
+    @Operation(summary = "get information of a planet")
     @GetMapping("/{id}")
     public ResponseEntity<PlanetDto> get(@PathVariable
                                          @Parameter(name = "id", example = "40b3ebd5-2475-42ec-8e50-38384ae7d80b")
                                          UUID id);
-
+    @Operation(summary = "delete a planet")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable
                                        @Parameter(name = "id", example = "40b3ebd5-2475-42ec-8e50-38384ae7d80b")
